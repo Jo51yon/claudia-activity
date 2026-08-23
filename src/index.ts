c79da274d04cfc064990b36e90a766b831ca5da0
Jo51yon <jolyon.grace@gracenet.co.uk>
2026-08-23 21:02:40 +0100

    initial: src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { logClaudiaActivity } from './logClaudiaActivity';
+export type { LogClaudiaActivityParams } from './logClaudiaActivity';
+export { default as ClaudiaActivityTimeline } from './ClaudiaActivityTimeline';
+export type { ClaudiaActivityTimelineProps, ClaudiaActivityTimelineCopy } from './ClaudiaActivityTimeline';
+export type { ClaudiaActivity } from './types';
